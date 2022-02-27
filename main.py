80bda26a024b7d6e308e17b2b56dc1444bd47ebd
fcmdhyd <fcmdhyd@gmail.com>
2022-02-27 21:00:06 +0800

    update ball.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@
 
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -17,6 +19,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(l_paddle.go_up, "w")
@@ -26,7 +29,9 @@ screen.onkey(r_paddle.go_down, "Down")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
